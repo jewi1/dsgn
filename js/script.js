9ad0b34f00b0b00570b1408d88a5d1e952fb2720
Slava <slava.89989@gmail.com>
2020-11-16 17:07:38 +0500

    Ветка для хостинга

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const hambourger = document.querySelector('.hambourger');
-
-hambourger.addEventListener('click', function (e) {
-  e.target.closest('.hambourger').classList.toggle('hambourger--active');
-  e.target.closest('.menu').querySelector('.menu__list').classList.toggle('menu__list--active');
-});
